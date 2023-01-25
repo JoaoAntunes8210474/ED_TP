bf15da931ffb4b5c0c800ba50ef6801ec3fcee0d
ReginaNeto <116004474+ReginaNeto@users.noreply.github.com>
2023-01-25 15:27:00 +0000

    Continuação dos comentários
    Continuação dos comentários

--- Projeto_ED_GRUPO14/src/main/java/api/interfaces/IConnector.java
+++ Projeto_ED_GRUPO14/src/main/java/api/interfaces/IConnector.java
@@ -6,18 +6,38 @@ import collections.implementation.ArrayUnorderedList;
 
 /**
  *
- * 
+ * Contract of a Connector Class
  */
 public interface IConnector extends ILocal{
     
+     /**
+     * get the specific time interval that the connector supplies power after interaction
+     * @return specific time interval that the connector supplies power after interaction
+     */
     public int getCooldown();
 
+    /**
+     * Get a set of players that have recently interacted with the connector
+     * @return players who have recently interacted with the connector
+     */
     public ArrayUnorderedList<Player> getPlayers();
 
+    /**
+     * Change the specific time interval that the connector supplies power after interaction
+     * @param cooldown the specific time interval that the connector supplies power after interaction
+     */
     public void setCooldown(int cooldown);
 
+    /**
+     * Change the set of players that have recently interacted with the connector
+     * @param players set of players who have recently interacted with the connector
+     */
     public void setPlayers(ArrayUnorderedList<Player> players);
 
+    /**
+     * String representing a connector
+     * @return String representing a connector
+     */
     @Override
     public String toString();
 
--- Projeto_ED_GRUPO14/src/main/java/api/interfaces/ILocal.java
+++ Projeto_ED_GRUPO14/src/main/java/api/interfaces/ILocal.java
@@ -5,19 +5,38 @@ import api.implementation.Coordinates;
 
 /**
  *
- * 
+ * Contract of a Local Class.
  */
 public interface ILocal {
    
-   
+    /**
+     * Get unique location identification number.
+     * @return The integer representing the unique identifier of each location
+     */
     public int getId();
 
+    /**
+     * Get place name.
+     * @return The location name, the name will be the name of points of interest like statues, churches ...
+     */
     public String getName();
 
+    /**
+     * Get location type
+     * @return The location type, can be portal or connector type.
+     */
     public String getLocalType();
 
+    /**
+     * Get amount of energy the location has.
+     * @return The amount of energy the site contains.
+     */
     public int getAmountEnergyItHas();
 
+    /**
+     * Get location coordinates.
+     * @return The location coordinates
+     */
     public Coordinates getCoordinates();
 
     public void setId(int id);
